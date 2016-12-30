4bb3837e8485da5dd9a1d56e601968313b93f101
Victoria Frantsuh <eboko1@gmail.com>
2016-12-30 12:28:45 +0200

    Initial commit

---
+++ src/Drop.java
@@ -0,0 +1,19 @@
+
+
+/**
+ * Created by Vika on 30.12.2016.
+ */
+public class Drop {
+    float x= 640/2;
+    float y=0;
+    float yspeed=1;
+
+    void fall(){
+        y=y+yspeed;
+    }
+
+    void show(){}
+
+
+
+}
---
+++ src/RainMain.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Vika on 30.12.2016.
+ */
+public class RainMain extends JFrame {
+    JPanel p = new JPanel();
+    Drop d;
+    public static void main (String arg[]){
+        new RainMain();
+    }
+
+    public RainMain(){
+        super ("Blue RAIN ");
+        setSize(640,360);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        add(p);
+        setVisible(true);
+    }
+    public  void setup(){
+        d=new Drop();
+    }
+    public void draw(){
+        setBackground(230,230,250);
+    }
+
+    private void setBackground(int a,int b, int c) {
+
+    }
+
+
+
+
+
+
+
+
+
+
+/*    public static final int WIDTH=640;
+    public static final int HEIGHT=360;
+
+    Drop d = new Drop();
+    public static void main(String arg[]){
+        JFrame f = new JFrame("Blue Rain ");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(WIDTH,HEIGHT);
+        f.setBackground(Color.CYAN);
+        //f.add();
+        f.setVisible(true);
+        f.add(new Drop());
+    }*/
+
+}
