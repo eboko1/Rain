3f70fde06a9a49fcfc77b23482d668022b267160
Victoria Frantsuh <eboko1@gmail.com>
2016-12-30 19:48:57 +0200

    add method

--- src/Drop.java
+++ src/Drop.java
@@ -1,22 +1,27 @@
+import javax.swing.*;
 import java.awt.*;
 
 /**
  * Created by Vika on 30.12.2016.
  */
-public class Drop {
-    int x = 640 / 2;
-    int y = 0;
-    int yspeed = 1;
+public class Drop extends JPanel{
+    public final int WIDTH=640;
+    int x=WIDTH/2;
+    int y=0;
+    int yspeed=1;
 
-    public void fall() {
-        y = y + yspeed;
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        this.setBackground(new Color(230,238,250));
+
+        g.setColor(new Color(138,43,226));
+        g.drawLine(x,y,x,y+10);
+
+
     }
 
-    public void show(Graphics g) {
-        g.setColor(new Color(138, 43, 226));
-       g.drawLine(y, x, x, y + 10);
 
-    }
+
 }
 
 
